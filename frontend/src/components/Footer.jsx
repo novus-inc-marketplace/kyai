ebd42b2f1b1209d373834e4cfde3a2a193847935
novus-inc-marketplace <novusincmarketplace@gmail.com>
2025-11-14 09:59:06 +0300

    configuring site for deployment

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,32 +3,32 @@ import { FaFacebookF, FaTwitter, FaYoutube } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="text-center mt-[30px]">
+    <footer className="text-center mt-[30px] bg-cream py-6">
       <ul className="inline-flex list-none p-0 m-[20px] space-x-[15px]">
         <li>
           <a href="#">
-            <span className="w-[50px] h-[50px] bg-icon-bg flex items-center justify-center text-white text-[30px] rounded-full">
+            <span className="w-[50px] h-[50px] bg-icon-bg flex items-center justify-center text-cream text-[30px] rounded-full">
               <FaFacebookF />
             </span>
           </a>
         </li>
         <li>
           <a href="#">
-            <span className="w-[50px] h-[50px] bg-icon-bg flex items-center justify-center text-white text-[30px] rounded-full">
+            <span className="w-[50px] h-[50px] bg-icon-bg flex items-center justify-center text-cream text-[30px] rounded-full">
               <FaTwitter />
             </span>
           </a>
         </li>
         <li>
           <a href="#">
-            <span className="w-[50px] h-[50px] bg-icon-bg flex items-center justify-center text-white text-[30px] rounded-full">
+            <span className="w-[50px] h-[50px] bg-icon-bg flex items-center justify-center text-cream text-[30px] rounded-full">
               <FaYoutube />
             </span>
           </a>
         </li>
       </ul>
-      <span className="copyright text-[22px] mb-[20px] block">
-        &#169; Untitled . All rights reserved. Design By <a href="http://templatestock.co" className="text-blue-500">Templatestock.co</a>
+      <span className="copyright text-[18px] text-neutral mb-[10px] block">
+        &#169; Barakah Roots. All rights reserved.
       </span>
     </footer>
   );
